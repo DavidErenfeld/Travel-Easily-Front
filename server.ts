cdc52707dc80f19aa723dc5d1d8bb3124bb8776b
David Erenfeld <1020dudu@gmail.com>
2024-03-15 10:45:46 +0200

    Preparation for deployment 3

---
+++ server.ts
@@ -0,0 +1,18 @@
+import express from "express";
+import path from "path";
+
+const app = express();
+const PORT = 80; // שימוש בפורט 80
+
+// הגדרת תיקיית ה'assets' כתיקייה סטטית
+app.use(express.static(path.join(__dirname, "dist")));
+
+// נתיב כללי שמחזיר את הדף הראשי לכל בקשת GET
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
+});
+
+// האזנה לפורט 80
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
