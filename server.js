3e9ee2cd058c92410b13e7b7185421e2ef0902b9
David Erenfeld <1020dudu@gmail.com>
2024-03-18 12:43:41 +0200

    Fixes in the server.js file

--- server.js
+++ server.js
@@ -1,30 +1,26 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-import express_1 from "express";
-import dotenv_1 from "dotenv";
-import fs_1 from "fs";
-import https_1 from "https";
-import path_1 from "path";
-dotenv_1.config();
-var app = (0, express_1)();
-var _a = process.env,
-  SSL_KEY_PATH = _a.SSL_KEY_PATH,
-  SSL_CERT_PATH = _a.SSL_CERT_PATH,
-  HTTPS_PORT = _a.HTTPS_PORT;
+const express = require("express");
+const dotenv = require("dotenv");
+const fs = require("fs");
+const https = require("https");
+const path = require("path");
+dotenv.config();
+const app = express();
+const { SSL_KEY_PATH, SSL_CERT_PATH, HTTPS_PORT } = process.env;
 if (!SSL_KEY_PATH || !SSL_CERT_PATH || !HTTPS_PORT) {
-  console.error("One or more environment variables are missing.");
-  process.exit(1);
+    console.error("One or more environment variables are missing.");
+    process.exit(1);
 }
-app.use(express_1.static(path_1.join(__dirname, "dist")));
-app.get("*", function (_req, res) {
-  res.sendFile(path_1.join(__dirname, "dist", "index.html"));
+app.use(express.static(path.join(__dirname, "dist")));
+app.get("*", (_req, res) => {
+    res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
-var httpsOptions = {
-  key: fs_1.readFileSync(SSL_KEY_PATH),
-  cert: fs_1.readFileSync(SSL_CERT_PATH),
+const httpsOptions = {
+    key: fs.readFileSync(SSL_KEY_PATH),
+    cert: fs.readFileSync(SSL_CERT_PATH),
 };
-https_1.createServer(httpsOptions, app).listen(HTTPS_PORT, function () {
-  console.log(
-    "Server running in production on https://localhost:".concat(HTTPS_PORT)
-  );
+https.createServer(httpsOptions, app).listen(HTTPS_PORT, () => {
+    console.log(`Server running in production on https://localhost:${HTTPS_PORT}`);
 });
+
